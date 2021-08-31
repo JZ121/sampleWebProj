8e5d2b548dea17e0b89afee859f86a4cde320eca
jafar <jafar@LAPTOP-CSFTBKK9>
2021-08-31 23:43:36 +0530

    first committ

---
+++ sampleWebProj/src/main/java/com/example/samplewebproj/SampleWebProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplewebproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebProjApplication.class, args);
+	}
+
+}
---
+++ sampleWebProj/src/test/java/com/example/samplewebproj/SampleWebProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplewebproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
