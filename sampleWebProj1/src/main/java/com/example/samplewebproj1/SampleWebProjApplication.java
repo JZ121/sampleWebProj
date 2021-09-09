7710ea1588f18bd9cef67d19e5d11d6e2355ff54
jafar <jafar@LAPTOP-CSFTBKK9>
2021-09-09 17:41:04 +0530

    first commit

---
+++ sampleWebProj1/src/main/java/com/example/samplewebproj1/SampleWebProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplewebproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebProjApplication.class, args);
+	}
+
+}
---
+++ sampleWebProj1/src/test/java/com/example/samplewebproj1/controller/HelloWorldControllerIntegTest.java
@@ -0,0 +1,33 @@
+package com.example.samplewebproj1.controller;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerIntegTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test 
+	public void hello() throws Exception {
+		RequestBuilder req=MockMvcRequestBuilders.get("/hello");
+		MvcResult result = mvc.perform(req).andReturn();
+		assertEquals("Hello Jafar V6", result.getResponse().getContentAsString());
+	
+	}
+	
+	
+}
